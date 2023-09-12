80f44e4c52818d6bb6883f79733024eb56abe2fe
Duaa <duaasayed74@gmail.com>
2023-09-12 16:29:20 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,12 @@
  * main - Entry point
  *
  * Description: Prints "_putchar" followed by a newline.
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-	putchar('');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
