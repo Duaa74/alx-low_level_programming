bbb8d1bc5313342effabbb436fc56b0c5f277cd6
Duaa <duaasayed74@gmail.com>
2023-09-12 18:48:57 -0700

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+* _islower - check if the letter is small,
+* @c: passed character as ascii code represented as int
+* Return: Returns either 0 or 1
 */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
 	return (0);
 }
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character.
- * @c: the character to check.
+ * main - check the code.
  *
- * Return: 1 if c is lowercase, 0 otherwise.
+ * Return: Always 0.
 */
-int _islower(int c)
+int main(void)
 {
-	if (c >= 'a' && c <= 'z')
-	return (1);
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
