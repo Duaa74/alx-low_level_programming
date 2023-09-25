64ac8dd8535d0d42ce0968b0ddc2dce9ae584911
Duaa <duaasayed74@gmail.com>
2023-09-25 07:10:43 -0700

    d

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "man.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: rows
+ *
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int j;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[c][j]);
+		_putchar('\n');
+	}
+}
