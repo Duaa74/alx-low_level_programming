c932a60e41f25275d4e161237d25214773185f87
Duaa74 <duaasayed74@gmail.com>
2023-10-19 12:20:58 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: pointer
+ *
+ * Return: the number of the node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
