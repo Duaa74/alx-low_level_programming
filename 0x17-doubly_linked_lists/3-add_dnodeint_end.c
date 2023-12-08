7ffe29ab18df12ad10aaf7a717140be6d0321ee6
Duaa74 <duaasayed74@gmail.com>
2023-12-09 02:01:37 +0300

    h

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint_end - add a node in the end
+ * @head: the head of the list
+ * @n: pointer
+ * Return: new element
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *start;
+
+	start = *head;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		new->n = n;
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	while (start->next)
+	{
+		start = start->next;
+	}
+	start->next = new;
+	new->prev = start;
+	new->next = NULL;
+	new->n = n;
+	return (new);
+}
