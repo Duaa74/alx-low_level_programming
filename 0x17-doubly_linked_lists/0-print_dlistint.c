a89378b1c8dc6c779c06d15246861593124709ca
Duaa74 <duaasayed74@gmail.com>
2023-12-09 01:22:06 +0300

    c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a list
+ * @h: pointer
+ * Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
