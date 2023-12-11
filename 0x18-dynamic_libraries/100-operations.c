6d02eaba86b63f6f4ae90dafa59a33266adb3bb6
Duaa74 <duaasayed74@gmail.com>
2023-12-12 01:33:50 +0300

    cc

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+/**
+ * add - add tow integers
+ * @a: first int
+ * @b: second int
+ * Return: add operators
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract tow integers
+ * @a: first int
+ * @b: second int
+ * Return: sub operators
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multebul tow integers
+ * @a: first int
+ * @b: second int
+ * Return: mul operators
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - modules tow integers
+ * @a: first int
+ * @b: second int
+ * Return: mod operators
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * div - divide tow integers
+ * @a: first int
+ * @b: second int
+ * Return: division operators
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
